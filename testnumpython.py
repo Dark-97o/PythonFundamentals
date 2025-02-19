b054a604cdcbb5ee0769a6da6e6483d2357443fa
Subh97-0 <114503763+Dark-97o@users.noreply.github.com>
2025-02-19 08:09:00 +0530

    Refactor output messages and improve code readability in testnumpython.py

--- testnumpython.py
+++ testnumpython.py
@@ -1,20 +1,21 @@
+# This file is created to test the numpy and math libraries in Python.
+# The math library is used for mathematical operations and the numpy library is used for numerical operations
+
 import math
 import numpy as np
 print(math.ceil(12.9))
 print(math.floor(10.1))
-print (math.fabs(686.9787998))
+print (math.fabs(686.798))
 print (math.factorial(9))
 print (math.frexp(878))
 print (math.isnan (float(78.9)))
-print (math.isnan (math.nan))
-print (math.isnan (math.inf))
 
 arr = np.array([34,2,65,23])
 print("NumPy Array:", arr)
 print("Sum of Array:", np.sum(arr))
 print("Mean of Array:", np.mean(arr))
 print("Maximum Value:", np.max(arr))
-print("Index of Maximum Value:", np.argmax(arr))
+print("Index of Max Value:", np.argmax(arr))
 
 matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print("\nNumPy 2D Array (Matrix):\n", matrix)
