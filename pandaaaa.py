428f97930568fc44aa3f971217bbdbbbeccd929f
Subh97-0 <114503763+Dark-97o@users.noreply.github.com>
2025-03-05 15:25:51 +0530

    Refactor line plot code into a separate function in pandaaaa.py

--- pandaaaa.py
+++ pandaaaa.py
@@ -22,13 +22,15 @@ print(df)
 print("\nSummary Statistics:")
 print(df.describe())
 
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 5, 7, 11]
-plt.plot(x, y)
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Line Plot')
-plt.show()
+def line_plot():
+        import matplotlib.pyplot as plt
+        x = [1, 2, 3, 4, 5]
+        y = [2, 3, 5, 7, 11]
+        plt.plot(x, y)
+        plt.xlabel('X-axis')
+        plt.ylabel('Y-axis')
+        plt.title('Line Plot')
+        plt.show()
 
 def scatter_plot():
         import matplotlib.pyplot as plt
