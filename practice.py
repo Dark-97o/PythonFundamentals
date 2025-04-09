1717ed7b43fda07ba3041bb60206e4b8c27f5311
Subh97-0 <114503763+Dark-97o@users.noreply.github.com>
2025-04-09 07:13:08 +0530

    Refactor practice.py to uncomment data loading example and improve code organization

--- practice.py
+++ practice.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
+np.random.seed(0)
+data = np.random.randn(500)
+# from sklearn.datasets import load_iris
 
-iris = load_iris()
-df = pd.DataFrame(iris.data,columns=iris.feature_names)
-df['target'] = iris.target
-print(df)
+# iris = load_iris()
+# df = pd.DataFrame(data=iris.data, columns = iris.feature_names)
+# df['target'] = iris.target
+# print(df)
 
 # data = { 'Name': ['Subhranil','Abhishek'],
 #         'Age':[30,46],
@@ -42,3 +44,25 @@ print(df)
 # df = pd.read_csv('https://people.sc.fsu.edu/~jburkardt/data/csv/airtravel.csv')
 # print(df)
 
+x = [1,2,3,4,5]
+y = [2,3,5,7,11]
+Categories = ['A','B','C','D']
+values = [20,30,25,35]
+
+plt.subplot(1,4,1)
+plt.plot(x,y)
+plt.title('Line Plot')
+
+plt.subplot(1,4,2)
+plt.bar(x,y)
+plt.title('Bar Plot')
+
+plt.subplot(1,4,3)
+plt.scatter(Categories,values)
+plt.title('Scatter Plot')
+
+plt.subplot(1,4,4)
+plt.hist(data,bins=30)
+plt.title('Histogram')
+
+plt.show()
